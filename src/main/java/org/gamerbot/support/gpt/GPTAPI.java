c1ccf01d3a6f8796be3fd535b6dad6b6463556ef
sammybernau <sambernau04@gmail.com>
2024-01-23 18:43:52 -0600

    Removed deactivated API key

--- src/main/java/org/gamerbot/support/gpt/GPTAPI.java
+++ src/main/java/org/gamerbot/support/gpt/GPTAPI.java
@@ -2,7 +2,7 @@ package org.gamerbot.support.gpt;
 
 public class GPTAPI {
 
-    public static String GPT_API_KEY = "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";
+    public static String GPT_API_KEY = "";
 
     public static GPTResponse postToGPT(String prompt, GPTModel model) {
         if (model == GPTModel.GPT_IMAGES) {
